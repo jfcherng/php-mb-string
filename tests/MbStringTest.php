37957e25d30e9601aea270e2c51d47128957d248
Jack Cherng <jfcherng@gmail.com>
2019-05-06 18:45:57 +0800

    $ composer fix
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- tests/MbStringTest.php
+++ tests/MbStringTest.php
@@ -9,8 +9,10 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @coversNothing
+ *
+ * @internal
  */
-class MbStringTest extends TestCase
+final class MbStringTest extends TestCase
 {
     /**
      * Test MbString.
@@ -41,58 +43,58 @@ class MbStringTest extends TestCase
         $str = '那我就測試一下許功蓋！';
         $mb = new MbString($str, 'UTF-8');
 
-        $this->assertSame('那我就測試一下許功蓋！', (string) $mb);
-        $this->assertSame('許', $mb[7]);
-        $this->assertSame('功', $mb->getAt(8));
-        $this->assertSame(11, $mb->strlen());
-        $this->assertSame(11, \count($mb));
-        $this->assertTrue(isset($mb[10]));
-        $this->assertFalse(isset($mb[11]));
+        static::assertSame('那我就測試一下許功蓋！', (string) $mb);
+        static::assertSame('許', $mb[7]);
+        static::assertSame('功', $mb->getAt(8));
+        static::assertSame(11, $mb->strlen());
+        static::assertSame(11, \count($mb));
+        static::assertTrue(isset($mb[10]));
+        static::assertFalse(isset($mb[11]));
 
         $mb->setAt(9, '德');
-        $this->assertSame('那我就測試一下許功德！', $mb->get());
+        static::assertSame('那我就測試一下許功德！', $mb->get());
 
         $mb->set('下一位 5 號是金城武。');
-        $this->assertSame('下一位 5 號是金城武。', $mb->get());
-        $this->assertSame('金城武', $mb->substr(8, 3));
+        static::assertSame('下一位 5 號是金城武。', $mb->get());
+        static::assertSame('金城武', $mb->substr(8, 3));
 
         // in-place substr_replace
         $mb->set('下一位 5 號是金城武。');
         $mb->substr_replace_i('不是許功蓋', 8, 3);
-        $this->assertSame('下一位 5 號是不是許功蓋。', $mb->get());
-        $this->assertSame(7, $mb->strpos('是'));
-        $this->assertSame(9, $mb->strpos('是', 8));
+        static::assertSame('下一位 5 號是不是許功蓋。', $mb->get());
+        static::assertSame(7, $mb->strpos('是'));
+        static::assertSame(9, $mb->strpos('是', 8));
 
         // in-place str_replace
         $mb->set('下一位一號是金城武。');
         $mb->str_replace_i('一', '七');
-        $this->assertSame('下七位七號是金城武。', $mb->get());
+        static::assertSame('下七位七號是金城武。', $mb->get());
 
         $mb->set('下一位 5 號是金城武。');
-        $this->assertTrue($mb->has('號'));
-        $this->assertFalse($mb->has('劉'));
-        $this->assertTrue($mb->startsWith('下一'));
-        $this->assertFalse($mb->startsWith('上'));
-        $this->assertTrue($mb->endsWith('。'));
-        $this->assertFalse($mb->endsWith('金城武'));
+        static::assertTrue($mb->has('號'));
+        static::assertFalse($mb->has('劉'));
+        static::assertTrue($mb->startsWith('下一'));
+        static::assertFalse($mb->startsWith('上'));
+        static::assertTrue($mb->endsWith('。'));
+        static::assertFalse($mb->endsWith('金城武'));
 
         $mb->set('下一位 5 號是金城武。');
         $mb->str_insert_i('也', 7);
-        $this->assertSame('下一位 5 號也是金城武。', $mb->get());
+        static::assertSame('下一位 5 號也是金城武。', $mb->get());
 
         $mb->set('今天是五月');
         $mb->append('十六日');
-        $this->assertSame('今天是五月十六日', $mb->get());
+        static::assertSame('今天是五月十六日', $mb->get());
 
         $mb->set('明年是2018');
         $mb->str_enclose_i(['「', '」'], 3, 4);
-        $this->assertSame('明年是「2018」', $mb->get());
+        static::assertSame('明年是「2018」', $mb->get());
 
         $mb->set('本月是五月');
-        $this->assertSame(['本', '月', '是', '五', '月'], $mb->toArray());
+        static::assertSame(['本', '月', '是', '五', '月'], $mb->toArray());
 
         $mb->set('本月，是五月。');
-        $this->assertSame(
+        static::assertSame(
             ['本月', '，', '是五月', '。', ''],
             $mb->toArraySplit('/([，。])/uS', -1, \PREG_SPLIT_DELIM_CAPTURE)
         );
